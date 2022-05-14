72bb42e183138375479dc36a0afed11a40228f5e
nullwullf <nwolf2@oswego.edu>
2022-05-14 11:13:19 -0400

    commented all property related code out

--- src/main/java/edu/oswegofs/mapdataserver/Property.java
+++ src/main/java/edu/oswegofs/mapdataserver/Property.java
@@ -1,42 +1,43 @@
-package edu.oswegofs.mapdataserver;
-
-import com.sun.istack.NotNull;
-import lombok.*;
-import javax.persistence.*;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-@Entity
-@Table(name = "Property")
-public class Property {
-
-    @Id
-    Long id;
-
-    @Column(name = "BuildingCode")
-    String buildingCode;
-
-    @Column(name = "Latitude")
-    Float latitude;
-
-    @Column(name = "Longitude")
-    Float longitude;
-
-    @Column(name = "BuildingName")
-    String buildingName;
-
-    @Column(name = "BuildAbr")
-    String buildAbr;
-
-    @Column(name = "Address")
-    String address;
-
-    @Column(name = "SquareFt")
-    Long squareFt;
-
-    public String getLatLong(){
-        return "[" + this.getLatitude() + "," + this.getLongitude() + "]";
-    }
-}
+//package edu.oswegofs.mapdataserver;
+//
+//import com.sun.istack.NotNull;
+//import lombok.*;
+//import javax.persistence.*;
+//
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "Property")
+//public class Property {
+//
+//    @Id
+//    @Column(name = "BuildingCode", nullable = false)
+//    String buildingCode;
+//
+//    @Column(name = "Latitude")
+//    Float latitude;
+//
+//    @Column(name = "Longitude")
+//    Float longitude;
+//
+//    @Column(name = "BuildingName")
+//    String buildingName;
+//
+//    @Column(name = "BuildAbr")
+//    String buildAbr;
+//
+//    @Column(name = "Address")
+//    String address;
+//
+//    @Column(name = "SquareFt")
+//    Long squareFt;
+//
+//    @Column(name = "AssetId")
+//    Long assetId;
+//
+//    public String getLatLong(){
+//        return "[" + this.getLatitude() + "," + this.getLongitude() + "]";
+//    }
+//}
